e7d05f6850c427319104223c3ef2f2e65b0affe3
Ilya Sterin <isterin@hotmail.com>
2002-01-31 18:11:24 +0000

    Import of ISTERIN/XML-SAXDriver-CSV-0.05 from CPAN.
    gitpan-cpan-distribution: XML-SAXDriver-CSV
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         ISTERIN/XML-SAXDriver-CSV-0.05.tar.gz
    gitpan-cpan-author:       ISTERIN
    gitpan-cpan-maturity:     released

--- CSV.pm
+++ CSV.pm
@@ -6,141 +6,121 @@ use Text::CSV_XS;
 
 use base qw(XML::SAX::Base);
 use vars qw($VERSION $NS_SAXDriver_CSV);
-$VERSION = '0.04';
+$VERSION = '0.05';
 $NS_SAXDriver_CSV = 'http://xmlns.perl.org/sax/XML::SAXDriver::CSV';
 
-
-#sub new {
-#    my ($class, %params) = @_;
-    
-#    return bless \%params, $class;
-#}
-
-sub parse {
+sub _parse_bytestream
+{
+  my $self = shift;
+  my $stream = shift; 
   
-    my $self = shift;
-    
-    my $args;
-    if (@_ == 1 && !ref($_[0])) {
-        $args = { Source => { String => shift }};
-    }
-    else {
-        $args = (@_ == 1) ? shift : { @_ };
-    }
-    
-    my $parse_options = { %$self, %$args };
-    #$self->{ParseOptions} = $parse_options;
-    
-    #if (!defined($parse_options->{Source})
-    #    || !(
-    #        defined($parse_options->{Source}{String})
-    #        || defined($parse_options->{Source}{ByteStream})
-    #        || defined($parse_options->{Source}{SystemId})
-    #        )) {
-    #    die "XML::SAXDriver::CSV: no source defined for parse\n";
-    #}
-    #
-    #if (defined($parse_options->{Handler})) {
-    #   $parse_options->{DocumentHandler} ||= $parse_options->{Handler};
-    #    $parse_options->{DTDHandler} ||= $parse_options->{DTDHandler};
-    #}
-    
-     
-    $parse_options->{NewLine} = "\n" unless defined($parse_options->{NewLine});
-    $parse_options->{IndentChar} = "\t" unless defined($parse_options->{IndentChar});
-        
-    $parse_options->{Parser} ||= Text::CSV_XS->new();
-    
-    $parse_options->{Declaration}->{Version} ||= '1.0';
-    
-    my ($ioref, @strings);
-    if (defined($parse_options->{Source}{SystemId}) 
-        || defined($parse_options->{Source}{ByteStream}) ) {
-        $ioref = $parse_options->{Source}{ByteStream};
-        if (!$ioref) {
-            require IO::File;
-            $ioref = IO::File->new($parse_options->{Source}{SystemId})
-                    || die "Cannot open SystemId '$parse_options->{Source}{SystemId}' : $!";
-        }
-        
-    }
-    elsif (defined $parse_options->{Source}{String}) {
-        @strings = split("\n", $parse_options->{Source}{String});
-    }
-    
-    my $document = {};
-    $self->start_document($document);
-    $self->xml_decl($parse_options->{Declaration});
-    my $pm_csv = $self->_create_node(
-                                    Prefix       => 'SAXDriver::CSV',
-                                    NamespaceURI => $NS_SAXDriver_CSV,
-                                    );
-    $self->start_prefix_mapping($pm_csv);
-    $self->end_prefix_mapping($pm_csv);
-    $self->characters({Data => $parse_options->{NewLine}});
-    
-    my $doc_element = {
-                Name => $parse_options->{File_Tag} || "records",
-                Attributes => {},
-            };
-
-    $self->start_element($doc_element);
-    $self->characters({Data => $parse_options->{NewLine}});
-    
-
-    $parse_options->{Col_Headings} ||= [];
-    #my @col_headings = @{$parse_options->{Col_Headings}};
-    
-    $parse_options->{Headings_Handler} ||= \&normalize_heading;
-             
-    while (my $row = get_row($parse_options->{Parser}, $ioref, \@strings)) {
-        my $el = {
-            Name => $parse_options->{Parent_Tag} || "record",
-            Attributes => {},
-        };
-        $self->characters(
-                {Data => $parse_options->{IndentChar}}
-        );
-        $self->start_element($el);
-        $self->characters({Data => $parse_options->{NewLine}});
-        
-        
-        if (!@{$parse_options->{Col_Headings}} && !$parse_options->{Dynamic_Col_Headings}) 
-        {
-                my $i = 1;
-                @{$parse_options->{Col_Headings}} = map { "column" . $i++ } @$row;
-        }
-        elsif (!@{$parse_options->{Col_Headings}} && $parse_options->{Dynamic_Col_Headings})
-        {
-                @{$parse_options->{Col_Headings}} = map { $parse_options->{Headings_Handler}->($_, $parse_options->{SubChar}); } @$row; 
-                next; # causes the first (heading) row to be skipped 
-                            
-        }   
-    
-        for (my $i = 0; $i <= $#{$row}; $i++) {
-            my $column = { Name => $parse_options->{Col_Headings}->[$i], Attributes => {} };
-            $self->characters(
-                    {Data => $parse_options->{IndentChar}} 
-            );
-            $self->start_element($column);
-            $self->characters({Data => $row->[$i]});
-            $self->end_element($column);
-            $self->characters({Data => $parse_options->{NewLine}});
-        }
-
-        $self->characters(
-                {Data => $parse_options->{IndentChar}}
-        );
-        $self->end_element($el);
-        $self->characters({Data => $parse_options->{NewLine}});
-    }
-
-    $self->end_element($doc_element);
-    
-    return $self->end_document($document);
+  $self->_parse_CSV(\$stream);  
+}
+sub _parse_string
+{
+  my $self = shift;
+  my $stream = shift; 
+  
+  my @strings = split("\n", $self->{ParseOptions}->{Source}{String});
+  $self->_parse_CSV(\@strings);  
+}
+sub _parse_systemid
+{
+  my $self = shift;
+  my $path = shift;
+  
+  require IO::File;
+  my $ioref = IO::File->new($self->{ParseOptions}->{Source}{SystemId})
+               || die "Cannot open SystemId '$self->{ParseOptions}->{Source}{SystemId}' : $!";
+       
+  $self->_parse_CSV($ioref);  
 }
 
-sub normalize_heading  ### Default if no Headings_Handler is provided
+sub _parse_CSV 
+{
+  
+  my $self = shift;
+  my $source = shift;
+  
+  $self->{ParseOptions}->{NewLine} = "\n" unless defined($self->{ParseOptions}->{NewLine});
+  $self->{ParseOptions}->{IndentChar} = "\t" unless defined($self->{ParseOptions}->{IndentChar});
+      
+  $self->{ParseOptions}->{Parser} ||= Text::CSV_XS->new();
+  
+  $self->{ParseOptions}->{Declaration}->{Version} ||= '1.0';
+  
+  my $document = {};
+  $self->start_document($document);
+  $self->xml_decl($self->{ParseOptions}->{Declaration});
+  my $pm_csv = $self->_create_node(
+                                  Prefix       => 'SAXDriver::CSV',
+                                  NamespaceURI => $NS_SAXDriver_CSV,
+                                  );
+  $self->start_prefix_mapping($pm_csv);
+  $self->end_prefix_mapping($pm_csv);
+  $self->characters({Data => $self->{ParseOptions}->{NewLine}});
+  
+  my $doc_element = {
+              Name => $self->{ParseOptions}->{File_Tag} || "records",
+              Attributes => {},
+          };
+
+  $self->start_element($doc_element);
+  $self->characters({Data => $self->{ParseOptions}->{NewLine}});
+  
+
+  $self->{ParseOptions}->{Col_Headings} ||= [];
+    
+  $self->{ParseOptions}->{Headings_Handler} ||= \&_normalize_heading;
+           
+  while (my $row = _get_row($self->{ParseOptions}->{Parser}, $source)) {
+      my $el = {
+          Name => $self->{ParseOptions}->{Parent_Tag} || "record",
+          Attributes => {},
+      };
+      $self->characters(
+              {Data => $self->{ParseOptions}->{IndentChar}}
+      );
+      $self->start_element($el);
+      $self->characters({Data => $self->{ParseOptions}->{NewLine}});
+      
+      
+      if (!@{$self->{ParseOptions}->{Col_Headings}} && !$self->{ParseOptions}->{Dynamic_Col_Headings}) 
+      {
+              my $i = 1;
+              @{$self->{ParseOptions}->{Col_Headings}} = map { "column" . $i++ } @$row;
+      }
+      elsif (!@{$self->{ParseOptions}->{Col_Headings}} && $self->{ParseOptions}->{Dynamic_Col_Headings})
+      {
+              @{$self->{ParseOptions}->{Col_Headings}} = map { $self->{ParseOptions}->{Headings_Handler}->($_, $self->{ParseOptions}->{SubChar}); } @$row; 
+              next; # causes the first (heading) row to be skipped 
+                          
+      }   
+  
+      for (my $i = 0; $i <= $#{$row}; $i++) {
+          my $column = { Name => $self->{ParseOptions}->{Col_Headings}->[$i], Attributes => {} };
+          $self->characters(
+                  {Data => $self->{ParseOptions}->{IndentChar}} 
+          );
+          $self->start_element($column);
+          $self->characters({Data => $row->[$i]});
+          $self->end_element($column);
+          $self->characters({Data => $self->{ParseOptions}->{NewLine}});
+      }
+
+      $self->characters(
+              {Data => $self->{ParseOptions}->{IndentChar}}
+      );
+      $self->end_element($el);
+      $self->characters({Data => $self->{ParseOptions}->{NewLine}});
+  }
+
+  $self->end_element($doc_element);
+  
+  return $self->end_document($document);
+}
+
+sub _normalize_heading  ### Default if no Headings_Handler is provided
 { 
   my $heading= shift;
   my $sub_char = shift || '_'; 
@@ -151,21 +131,24 @@ sub normalize_heading  ### Default if no Headings_Handler is provided
   return $heading; 
 }
 
-sub get_row {
-    my ($parser, $ioref, $strings) = @_;
+sub _get_row {
+    my ($parser, $source, $strings) = @_;
     
-    if ($ioref) {
-        my $line = <$ioref>;
-        if ($line && $parser->parse($line)) {
-            return [$parser->fields()];
-        }
+    if (ref($source) eq "ARRAY")
+    {
+      my $line = shift @$source;
+      if ($line && $parser->parse($line)) {
+        return [$parser->fields()];
+      }
     }
-    else {
-        my $line = shift @$strings;
-        if ($line && $parser->parse($line)) {
-            return [$parser->fields()];
-        }
+    else
+    {
+      my $line = <$source>;
+      if ($line && $parser->parse($line)) {
+        return [$parser->fields()];
+      }
     }
+    
     return;
 }
 
@@ -193,7 +176,7 @@ __END__
 
 =head1 DESCRIPTION
 
-    XML::SAXDriver::CSV was developed as a complement to XML::CSV, though it provides a SAX
+    XML::SAXDriver::CSV was developed as a compliment to XML::CSV, though it provides a SAX
     interface, for gained performance and efficiency, to CSV files.  Specific object attributes
     and handlers are set to define the behavior of the parse() method.  It does not matter where 
     you define your attributes.  If they are defined in the new() method, they will apply to all
@@ -211,6 +194,14 @@ __END__
         SystemId - Contains the path to the file containing the CSV data. Ex (Source => {SystemId => '../csv/foo.csv'})
         
     Handler - Contains the object to be used as a XML print handler
+    
+    Declaration
+        
+        Version - Specifies an XML version for declaration.  Defaults to '1.0'.
+        
+        Encoding - Specifies the endcoding in XML declaration.  Omitted by default.
+        
+        Standalone - Specifies the standalone attribute.  Omitted by default.  
     
     DTDHandler - Contains the object to be used as a XML DTD handler.  
                  ****There is no DTD support available at this time.  
